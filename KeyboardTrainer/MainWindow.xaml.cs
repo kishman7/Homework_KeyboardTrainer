f225812312641b918e641dfc66b8d4eadd8a8a11
kishman7 <kishman_us@ukr.net>
2021-10-17 18:26:41 +0300

    Add Homework_KeyboardTrainer

---
+++ KeyboardTrainer/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace KeyboardTrainer
+{
+    internal sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
